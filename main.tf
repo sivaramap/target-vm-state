4d9e6ef67ca5836cc6227d7bf7ebf5d56f54e684
unknown <ishaqmd@live.com>
2020-03-23 22:51:34 +0530

    one

---
+++ main.tf
@@ -0,0 +1,30 @@
+# This is the provider used to spin up the gcloud instance
+provider "google" {
+  credentials = file("terraform.json")
+  project = "ishaqgcpproject"
+  region  = "us-east1"
+  
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version && git clone https://github.com/ishaqmdgcp/gce-startup.git && cd /gce-startup && terraform init && terraform plan && terraform apply -input=false -auto-approve"
+
+   
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
